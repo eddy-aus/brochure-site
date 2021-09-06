c6bf7391fcd427269c95a38836f57337baefa02e
Daniel Maddison <dmaddison@icloud.com>
2021-09-06 18:34:29 +1000

    BugFix

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -12,7 +12,6 @@ import Preloader from './components/preloader';
 import Nav from './layouts/nav';
 import Contact from './pages/contact';
 import Home from './pages/home';
-import NotFound from './pages/not-found';
 import { BEM } from './utils';
 import '../scss/styles.scss';
 
@@ -57,7 +56,6 @@ const App = () => {
             <Redirect exact from="/" to="/home" />
             <Route component={Home} path="/home" />
             <Route component={Contact} path="/contact" />
-            <Route component={NotFound} />
           </Switch>
         </Router>
       )}
