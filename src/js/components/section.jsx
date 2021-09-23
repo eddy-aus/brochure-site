29ee66b3a9c49c44bb74f5bb515b01fcc56083eb
Daniel Maddison <dmaddison@icloud.com>
2021-09-23 22:06:42 +1000

    Application updates

--- src/js/components/section.jsx
+++ src/js/components/section.jsx
@@ -4,48 +4,160 @@ import { BEM } from '../utils';
 
 const { block, element, modifier } = BEM('section');
 
-const ImageSection = (props) => {
-  const { children, heading, headingPunctuation, image } = props;
+const PrimarySection = (props) => {
+  const { children, heading, headingPunctuation, image, style } = props;
 
   return (
-    <section
-      className={block() + ' ' + modifier('image')}
-      style={{ backgroundImage: `url('${image.trim()}')` }}
-    >
+    <section className={block()} style={style}>
       <div className={element('inner')}>
         <h2 className={element('heading')}>
           {heading.toTitleCase()}
           <Punc>{headingPunctuation}</Punc>
         </h2>
-        <div className={element('blurb')}>{children}</div>
+        <div className={element('content')}>{children}</div>
+      </div>
+      <div className={element('inner')}>
+        <div className={element('artwork')}>
+          <svg
+            className={element('svg')}
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            viewBox="0 0 320 240"
+          >
+            <path
+              className={element('path')}
+              d="M212.339 58.5178C211.925 59.2352 212.171 60.1526 212.888 60.5668C213.605 60.981 214.523 60.7352 214.937 60.0178C215.351 59.3003 215.105 58.383 214.388 57.9688C213.671 57.5545 212.753 57.8003 212.339 58.5178ZM212.339 58.5178L159.944 28.2678"
+              vectorEffect="non-scaling-stroke"
+            />
+            <path
+              className={element('path')}
+              d="M107.651 58.5138C107.237 57.7964 106.319 57.5506 105.602 57.9648C104.885 58.379 104.639 59.2964 105.053 60.0138C105.467 60.7313 106.385 60.9771 107.102 60.5629C107.819 60.1487 108.065 59.2313 107.651 58.5138ZM107.651 58.5138L160.046 28.2638"
+              vectorEffect="non-scaling-stroke"
+            />
+            <path
+              className={element('path')}
+              d="M159.99 4.63243e-06V28.38"
+              vectorEffect="non-scaling-stroke"
+            />
+            <image
+              className={`${element('image')} ${modifier('s', 'image')}`}
+              height="auto"
+              href={image.trim() + '-320.png'}
+              width="106%"
+              x="-3%"
+              y="-22.25%"
+            />
+            <image
+              className={`${element('image')} ${modifier(
+                's',
+                'image',
+              )} ${modifier('2x', 'image')}`}
+              height="auto"
+              href={image.trim() + '-320-@2x.png'}
+              width="106%"
+              x="-3%"
+              y="-22.25%"
+            />
+            <image
+              className={`${element('image')} ${modifier('m', 'image')}`}
+              height="auto"
+              href={image.trim() + '-480.png'}
+              width="106%"
+              x="-3%"
+              y="-22.25%"
+            />
+            <image
+              className={`${element('image')} ${modifier(
+                'm',
+                'image',
+              )} ${modifier('2x', 'image')}`}
+              height="auto"
+              href={image.trim() + '-480-@2x.png'}
+              width="106%"
+              x="-3%"
+              y="-22.25%"
+            />
+            <image
+              className={`${element('image')} ${modifier('l', 'image')}`}
+              height="auto"
+              href={image.trim() + '-768.png'}
+              width="106%"
+              x="-3%"
+              y="-22.25%"
+            />
+            <image
+              className={`${element('image')} ${modifier(
+                'l',
+                'image',
+              )} ${modifier('2x', 'image')}`}
+              height="auto"
+              href={image.trim() + '-768-@2x.png'}
+              width="106%"
+              x="-3%"
+              y="-22.25%"
+            />
+            <path
+              className={element('path')}
+              d="M107.651 181.482C108.065 180.765 107.819 179.847 107.102 179.433C106.385 179.019 105.467 179.265 105.053 179.982C104.639 180.7 104.885 181.617 105.602 182.031C106.319 182.445 107.237 182.2 107.651 181.482ZM107.651 181.482L160.046 211.732"
+              vectorEffect="non-scaling-stroke"
+            />
+            <path
+              className={element('path')}
+              d="M212.339 181.486C212.753 182.203 213.671 182.449 214.388 182.035C215.105 181.621 215.351 180.703 214.937 179.986C214.523 179.269 213.605 179.023 212.888 179.437C212.171 179.851 211.925 180.769 212.339 181.486ZM212.339 181.486L159.944 211.736"
+              vectorEffect="non-scaling-stroke"
+            />
+            <path
+              className={element('path')}
+              d="M160 240V211.62"
+              vectorEffect="non-scaling-stroke"
+            />
+          </svg>
+        </div>
       </div>
     </section>
   );
 };
 
-const SolidSection = (props) => {
-  const { children, heading, headingPunctuation, image } = props;
+const SecondarySection = (props) => {
+  const { children, heading, headingPunctuation, style } = props;
 
   return (
-    <section className={block()}>
-      <div className={element('inner')}>
-        <h2 className={element('heading')}>
-          {heading.toTitleCase()}
-          <Punc>{headingPunctuation}</Punc>
-        </h2>
-        <div className={element('blurb')}>{children}</div>
-      </div>
-      <div
-        className={element('image')}
-        style={{ backgroundImage: `url('${image.trim()}')` }}
-      />
+    <section className={`${block()} ${modifier('secondary')}`} style={style}>
+      <h2 className={element('heading')}>
+        {heading.toTitleCase()}
+        <Punc>{headingPunctuation}</Punc>
+      </h2>
+      <div className={element('content')}>{children}</div>
+    </section>
+  );
+};
+
+const TertiarySection = (props) => {
+  const { children, heading, headingPunctuation, style } = props;
+
+  return (
+    <section className={`${block()} ${modifier('tertiary')}`} style={style}>
+      <h2 className={element('heading')}>
+        {heading.toTitleCase()}
+        <Punc>{headingPunctuation}</Punc>
+      </h2>
+      <div className={element('content')}>{children}</div>
     </section>
   );
 };
 
 const Section = (props) => {
   const { type, ...rest } = props;
-  const Component = type === 'image' ? ImageSection : SolidSection;
+
+  let Component = PrimarySection;
+
+  if (type === 'secondary') {
+    Component = SecondarySection;
+  }
+
+  if (type === 'tertiary') {
+    Component = TertiarySection;
+  }
 
   return <Component {...rest} />;
 };
@@ -57,13 +169,14 @@ Section.propTypes = {
   ]).isRequired,
   heading: PropTypes.string.isRequired,
   headingPunctuation: PropTypes.oneOf(['.', '?', '!', '...', ',']),
-  image: PropTypes.string.isRequired,
-  type: PropTypes.oneOf(['image', 'solid']),
+  image: PropTypes.string,
+  style: PropTypes.style,
+  type: PropTypes.oneOf(['primary', 'secondary', 'tertiary']),
 };
 
 Section.defaultProps = {
   headingPunctuation: '.',
-  type: 'solid',
+  type: 'primary',
 };
 
 export { Section as default };
--- src/js/pages/not-found.jsx
+++ src/js/pages/not-found.jsx
@@ -1,3 +1,12 @@
-const NotFound = () => <h1>Uh-oh! Not found.</h1>;
+import { Helmet } from 'react-helmet';
+
+const NotFound = () => (
+  <>
+    <Helmet>
+      <title>404 | Eddy Australia</title>
+    </Helmet>
+    <h1>Uh-oh! Not found.</h1>
+  </>
+);
 
 export { NotFound as default };
